93c76e78e99ea79fc0e5ad41d1caa1c1ac05c504
ShadeCodesYT <87731683+DarkestCodeYT@users.noreply.github.com>
2023-09-27 15:08:46 -0600

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,25 @@
-import requests, os
-path = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft','mods')
+import subprocess, pyautogui, requests, time, os
+path = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft')
 print(f'Default Path: {path}')
-folder_path = input('What path is your minecraft')
-if folder_path == '':
-  folder_path = path
+path = input('What path is your minecraft')
+if path == '':
+  path = path
+if os.path.exists(os.path.join(os.getcwd(),'locationtoclick.txt')):
+  #
 urls = input('url: ')
 if urls = '':
   urls = requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/server/mods.txt').text
 elif ',' in urls:
   urls = urls.split(',')
-for filename in os.listdir(folder_path):
-  file_path = os.path.join(folder_path, filename)
-  if os.path.isfile(file_path):
-    os.remove(file_path)
+for filename in os.listdir(os.path.join(path,'mods')):
+  if os.path.isfile(os.path.join(os.path.join(path,'mods'), filename)):
+    os.remove(os.path.join(os.path.join(path,'mods'), filename))
 for url in urls:
   response = requests.get(url)
-  with open(os.path.join(folder_path,url.split(r'/')[-1]),'wb') as file:
+  with open(os.path.join(path,url.split(r'/')[-1]),'wb') as file:
     file.write(response.content)
 # run minecraft
+time.sleep(10)
+subprocess.Popen(os.path.join(path,'MinecraftLauncher.exe'))
+time.sleep(10)
+pyautogui.click(x=mouse[0], y=mouse[1])
