654fcee26fa168da78e5fd61db1cf1d2b538ddd8
ShadeCodesYT <87731683+DarkestCodeYT@users.noreply.github.com>
2023-09-28 08:30:59 -0600

    Update server.py

--- server.py
+++ server.py
@@ -3,14 +3,8 @@ path = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft')
 print(f'Default Path: {path}')
 path = input('What path is your minecraft')
 if path == '':
-  path = path
-urls = input('url: ')
-if urls = '':
-  urls = requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/server.txt').text.split(',')
-elif ',' in urls.split(''):
-  urls = urls.split(',')
-else:
-  urls = requests.get(urls).text.split(',')
+  path = os.path.join(os.path.expanduser('~'),'AppData','Roaming','.minecraft')
+urls = requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/server.txt').text.split(',')
 for filename in os.listdir(os.path.join(path,'mods')):
   if os.path.isfile(os.path.join(os.path.join(path,'mods'), filename)):
     os.remove(os.path.join(os.path.join(path,'mods'), filename))
