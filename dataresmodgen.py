40c32d9e4dbb42aad19853e7fd7a2b5284935f91
ShadeCodesYT <87731683+DarkestCodeYT@users.noreply.github.com>
2023-10-03 12:42:32 -0600

    Update dataresmodgen.py

--- dataresmodgen.py
+++ dataresmodgen.py
@@ -1,28 +1,24 @@
 import requests
 import zipfile
 import os
-def loop_input(text):
+def loop_input(text,values=['_']):
   value = input(text)
   if value == '':
     loop_input(text)
   else:
-    return value
+    return value if '_' in values or value in values
 file_path = 'mod.jar'
 output = 'mod'
 modID = loop_input('modID> ')
+os.makedirs('fabric-'+output, exist_ok=True)
+os.makedirs('forge-'+output, exist_ok=True)
 username = loop_input('username> ')
+forge = loop_input('MineCraft Java Forge? Y or N\nAwnser> ',values=['Y','N'])
 if not os.path.exists(file_path):
-  with open(file_path,'wb') as file:
-    file.write(requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/mods/datapack-resourepack.jar').content)
-os.makedirs(output, exist_ok=True)
-with zipfile.ZipFile(file_path, 'r') as file:
-  file.extractall(output)
-os.remove(file_path)
-with open(os.path.join(os.path.join(output,'META-INF'),'mods.toml'),'r') as file:
-  lines = file.readlines()
-lines[7] = f'displayName="{modID}"\n'
-lines[10] = f'authors="{username}, ShadeCodesYT, MCreator"'
-with open(os.path.join(os.path.join(output,'META-INF'),'mods.toml'),'w') as file:
-  file.writelines(lines)
-  del lines
-#
+  with open('forge-'+file_path,'wb') as file:
+    file.write(requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/templates/forge.jar').content)
+  with open('fabric-'+file_path,'wb') as file:
+    file.write(requests.get('https://raw.githubusercontent.com/DarkestCodeYT/MODS/main/templates/forge.jar').content)
+with zipfile.ZipFile('forge-'+file_path, 'r') as file:
+  file.extractall('forge-'+output)
+os.remove('forge-'+file_path)
